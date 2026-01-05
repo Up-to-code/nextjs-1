6d602410482e58cc390945c3f4ffde0d7ad4350e
Up-to-code <133324agh@gmail.com>
2026-01-05 13:38:19 +0200

    0
    0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./global.css";
+import "./globals.css";
 import { Cairo } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import {
